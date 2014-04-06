279e3ce1f09ec3c41431d82b2100f32a78591618
flameiguana <flameiguana@gmail.com>
2014-04-05 20:08:11 -0700

    Added reference background, resized main camera to appropriate ratio. Web player should run at 448 x 512

---
+++ Assets/GameCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+
+	public Sprite background;
+
+	// Use this for initialization
+	void Start () {
+
+		//Takes the height of background sprite in world units.
+		float desiredHeightUnits = background.bounds.size.y;
+		//adjust camera to match this
+		camera.orthographicSize = desiredHeightUnits / 2.0f;
+		//print (camera.orthographicSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
