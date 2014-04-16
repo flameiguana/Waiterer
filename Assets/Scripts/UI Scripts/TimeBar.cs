cf1487c73e1f34b701128a7495ab8ff8717f102a
SeanMendenhall <semenden@ucsc.edu>
2014-04-15 20:24:04 -0700

    TimeBar added

---
+++ Assets/Scripts/UI Scripts/TimeBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeBar : MonoBehaviour {
+	public float time;
+	public UISlider timeBar;
+
+
+	// Use this for initialization
+	void Start () {
+		 time = 10;
+		 timeBar = gameObject.GetComponent<UISlider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time -= Time.deltaTime;
+		timeBar.sliderValue = time / 10;
+	}
+}
