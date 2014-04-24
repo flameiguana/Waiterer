5ffdc6f7779cd065e0c4f19e72d063d24e66ff5c
SeanMendenhall <semenden@ucsc.edu>
2014-04-24 13:41:12 -0700

    Credits Scene and Button functionality

---
+++ Assets/Scripts/UI Scripts/BackToMenuButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackToMenuButton : MonoBehaviour {
+
+	void OnPress(){
+		Application.LoadLevel("MainMenu");
+		Time.timeScale = 1f;
+	}
+}
---
+++ Assets/Scripts/UI Scripts/CreditsButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsButton : MonoBehaviour {
+	void OnPress(){
+		Application.LoadLevel("Credits");
+		Time.timeScale = 1f;
+	}
+}
